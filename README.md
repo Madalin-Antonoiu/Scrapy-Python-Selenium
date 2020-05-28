ea514569120f5c4b40713c97a5e42544d5a66f4c
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-05-28 02:28:27 +0100

    Updated readme to include most of code comments

--- README.md
+++ README.md
@@ -3,16 +3,15 @@ Logging via Forms into any website and scraping it with callbacks
 
 **How to start building spiders with Scrapy** :
 
-1 **Right Click on Desktop** - **Open with Code**   
-2 CTRL +' to **open Terminal (cmd) from VS Code**.  
+1 Right Click on Desktop - **Open with Code**   
+2 `CTRL +'` to **open VS Code Terminal (cmd) **.  
 3 `py -m venv venv` - create a desktop virtual environment for python  
 4 `.\venv\Scripts\activate` - to be in venv mode, you can now install packages locally to this project  
 5 `pip install scrapy` - Needs Microsoft 14++ installed, see my email sent to myself  
 
-See **login_spider**.py for how to log in to a website before crawling
-See **time**.py for how to format datetime to make time_ago time
-See **saving_spider**.py for how to save locally information to a file.
-
+See **login_spider**.py for how to log in to a website before crawling  
+See **time**.py for how to format datetime to make time_ago time  
+See **saving_spider**.py for how to save locally information to a file.  
 **PyCharm** ( needs Git installed for VSC) is really cool IDE for Python.
 
 **You know how to**:
@@ -21,3 +20,12 @@ See **saving_spider**.py for how to save locally information to a file.
 - Save data in csv and json format
 - Open browser with scraped data automatically
 - Extract any piece of information you want exactly with css selectors and Xpath
+
+**Other useful commands**:
+- `CTRL + Z + Enter` to terminate Scrapy shell
+- `//from scrapy.utils.response import open_in_browser  
+  //open_in_browser(response)` - auto open new Chrome window with scraped data
+- `scrapy shell 'https://your_link'` - cool to test things
+- Save scraped data as html document locally:  
+`with open('page.html', 'wb') as html_file:  
+              html_file.write(response.body)`
--- webscrape/webscrape/spiders/login_spider.py
+++ webscrape/webscrape/spiders/login_spider.py
@@ -1,14 +1,14 @@
+import datetime
 import scrapy
-import timeago, datetime
-
+import timeago
 from scrapy.http import FormRequest
 from scrapy.utils.response import open_in_browser
+
 
 class TicketItem(scrapy.Item):
     id = scrapy.Field()
     time_ago = scrapy.Field()
     subject = scrapy.Field()
-
 
 class LoginSpider(scrapy.Spider):
     name = "login_spider"
@@ -46,23 +46,11 @@ class LoginSpider(scrapy.Spider):
 
     @staticmethod
     def start_scraping(response):
-         #adding 3 minutes ago to
 
-
-
-
-
-        ### REMOVE TBODY FROM ANY XPATH ! WARNING
-
-
-       # print(response.xpath('/html/body/div/table/tr[3]/td[7]/div/a/text()').get()) # Works
-                                        #### Table Rows ####
+        ### REMOVE TBODY FROM ANY XPATH ! WARNING, else [] empty response
         for ticket in response.xpath('/html/body/div/table/tr[position() > 2]'): # don't put get or extract here, > 2 because first two are header rows
 
             item = TicketItem()
-
-
-            # This for is needed because every xpath.extract gives a list with all of them, and need parsed individually
 
             for date in ticket.xpath('normalize-space(./td[11]/div/text())').extract(): # date string grabbed from every xpath
                 datetime_object = datetime.datetime.strptime(date, '%d/%m/%Y %I:%M %p') # formatted into proper datetime date object (comes like '27/05/2020 12:17 PM' on the website)
@@ -75,82 +63,3 @@ class LoginSpider(scrapy.Spider):
             yield item
 
         open_in_browser(response)
-
-        # print(response) #.text
-        # first_ticket = response.selector.xpath('//*[@id="RequestsView_r_0_6"]/div/a/text()').get()
-        # print(first_ticket)
-
-        # table_rows = response.xpath('//*[@id="RequestsView_TABLE"]/tbody/tr').extract()
-        # print(table_rows)
-
-        # mydiv = response.xpath("//div").extract()
-        # print(mydiv)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # CTRL + Z + Enter to terminate Scrapy shell
-
-        # response.css('title')
-        #   response.css('#RequestsView_r_0_6 > div > a')
-        #
-        #
-        #
-        #
-        #
-        #
-
-        # Open in Browser
-        # open_in_browser(response)
-
-        # Write scrape result to HTML file
-        #with open('page.html', 'wb') as html_file:
-        #   html_file.write(response.body)
-
-
-#To run this program in the shell
-
-# scrapy shell https://servicedesk.csiltd.co.uk/RequestsView.cc?requestViewChanged=true&viewName=36502_MyView&globalViewName=All_Requests
-# then write this in the shell
-
- # def parse(self, response):
- #        user = 'antonoium'
- #        password = 'madutzu93classiC'
- #        token = response.css('form input::attr(value)').extract_first()      # print(token) #RememberMeLoginModule hidden value
- #        return FormRequest.from_response(response, formdata={
- #            'AUTHRULE_NAME': token,
- #            'j_username': user,
- #            'j_password': password,
- #            'dname': '0',
- #            'DOMAIN_NAME': '-',
- #            'LDAPEnable': 'false',
- #            'hidden': 'Select a Domain',
- #            'hidden': 'For Domain',
- #            'AdEnable': 'false',
- #            'DomainCount': '3',
- #            'LocalAuth': 'No',
- #            'LocalAuthWithDomain': 'Not in Domain',
- #            'dynamicUserAddition_status': 'true',
- #            'localAuthEnable': 'true',
- #            'logonDomainName': '-1',
- #            'checkbox': 'checkbox'
- #        }, callback=self.start_scraping)
- #
- #    def start_scraping(self, response):
-
- # then all you do is type response and you have all the code lol, and you are authenticated

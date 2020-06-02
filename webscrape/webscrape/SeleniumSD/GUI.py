1c8f6b2a792264b96b25942c8d36cc320c9beb25
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-06-02 21:41:11 +0100

    Added a LOT

--- webscrape/webscrape/SeleniumSD/GUI.py
+++ webscrape/webscrape/SeleniumSD/GUI.py
@@ -1,5 +1,31 @@
 import tkinter as tk
+from tkinter.ttk import Progressbar
 from webscrape.webscrape.SeleniumSD.automate_servicedesk import AutoSD # importing my own script class
+import threading
+import asyncio
+import time
+
+
+def submit(): # put your functions for new thread here
+    print('It is running')
+    bot = AutoSD()
+    bot.main()
+    #text1.insert(tk.INSERT, "Done")
+
+def start_submit_thread(event):
+    global submit_thread
+    submit_thread = threading.Thread(target=submit)
+    submit_thread.daemon = True
+    progressbar.start()
+    submit_thread.start()
+    window.after(20, check_submit_thread)
+
+def check_submit_thread():
+    if submit_thread.is_alive():
+        window.after(20, check_submit_thread)
+    else:
+        #progressbar.stop()
+        progressbar.stop()
 
 def close_app():
     window.destroy()
@@ -7,56 +33,90 @@ def close_app():
 def print_val():
     print(inputDc_entry.get())
 
-def run_app():
-    print('It is running')
-    bot = AutoSD()
-    bot.main()
-
-
-
 window = tk.Tk()
 window.title('CSI Quick Tools')
-window.geometry("600x600")
-window.resizable("true", "false") # (hor, ver.)
+window.geometry("420x600")
+window.resizable("false", "false") # (hor, ver.)
+
 
 # Splitting the window in 3 zones
-frame_header = tk.Frame(window, borderwidth=2, pady=2)
+frame_header = tk.Frame(window, borderwidth=2, pady=2, bg="#263D42")
 center_frame = tk.Frame(window, borderwidth=2, pady=5)
 bottom_frame = tk.Frame(window, borderwidth=2, pady=5)
 
 # Attributing them to grid
-frame_header.grid(row=0, column=0)
-center_frame.grid(row=1, column=0)
-bottom_frame.grid(row=2, column=0)
+frame_header.grid(row=0, column=2)
+center_frame.grid(row=1, column=2)
+bottom_frame.grid(row=2, column=2)
 
-#header = tk.Label(frame_header, text="Something", bg="grey", fg="black", height="3", width="20", font={"Helvetica 18 bold"})
-#header.grid(row="0", column="0")
+# Header
+header = tk.Label(frame_header, text="Other Tools", bg="grey", fg="black", height="1", width="34", font={"Helvetica 14 bold"})
+header.grid(row="0", column="0")
 
-# DC Frame and its display
+# Center frame split into two frames
 frame_main_1 = tk.Frame(center_frame, borderwidth=2, relief="sunken")
+frame_main_2 = tk.Frame(center_frame, borderwidth=2, relief="sunken")
 
-# DC Label and its display
+#Bottom frame too
+frame_bottom_1 = tk.Frame(bottom_frame, borderwidth=2)
+frame_bottom_2 = tk.Frame(bottom_frame, borderwidth=2)
+frame_bottom_3 = tk.Frame(bottom_frame, borderwidth=2)
+
+# First row - Domain Controller
 inputDc = tk.Label(frame_main_1, text="Domain Controller ")
 inputDc_StringVar = tk.StringVar()
 inputDc_entry = tk.Entry(frame_main_1, textvariable=inputDc, width="25") # this is what holds data
 inputDc_entry.insert(0, 'e.g masthaven niuadmin')
+domainController_button = tk.Button(frame_main_1, text="Open", command=print_val, bg="dark green", fg="white", relief="raised", width="10", font={"Helvetica 10 bold"})
 
-# Button 1
-button_run = tk.Button(frame_main_1, text="Open", command=print_val, bg="dark green", fg="white", relief="raised", width="10", font={"Helvetica 10 bold"})
+# VDI Broker
+vdiBroker = tk.Label(frame_main_2, text="VDI Broker")
+vdiBroker_StringVar = tk.StringVar()
+vdiBroker_entry = tk.Entry(frame_main_2, textvariable=vdiBroker, width="33") # this is what holds data
+vdiBroker_entry.insert(0, 'e.g cambridge')
+vdiBroker_button = tk.Button(frame_main_2, text="Open", command=print_val, bg="dark green", fg="white", relief="raised", width="10", font={"Helvetica 10 bold"})
 
-# Button 2
-button_run1 = tk.Button(bottom_frame, text="Group Unassigned", command=run_app, bg="orange", fg="white", relief="raised", width="15", font={"Helvetica 10 bold"})
-button_run1.grid(column=0, row=1, sticky="w", padx=5, pady=2)
 
-# Button 2
-button_run2 = tk.Button(bottom_frame, text="My Tickets", command=run_app, bg="orange", fg="white", relief="raised", width="15", font={"Helvetica 10 bold"})
-button_run1.grid(column=0, row=1, sticky="w", padx=5, pady=2)
+
+
+# Bottom row Frame1 - Buttons
+groupUnassigned = tk.Button(frame_bottom_1, text="Group Unassigned", command=submit, bg="orange", fg="white", relief="raised", width="15", font={"Helvetica 10 bold"})
+groupUnassigned.grid(row=2, column=0, sticky="w", padx=5, pady=2)
+
+myTickets = tk.Button(frame_bottom_1, text="My Tickets", command=print_val, bg="orange", fg="white", relief="raised", width="15", font={"Helvetica 10 bold"})
+myTickets.grid(row=2, column=1, sticky="w", padx=5, pady=2)
+
+# Bottom row Frame2 - Text Box
+text1 = tk.Text(frame_bottom_2, height=10, width=34)
+text1.config(state="disabled")
+
+
 
 #Packing the things visually
 frame_main_1.pack(fill="x", pady="2")
+frame_main_2.pack(fill="x", pady="2")
+
 inputDc.pack(side="left", padx="5")
 inputDc_entry.pack(side="left", padx="5")
-button_run.pack(side="left", padx="2")
+domainController_button.pack(side="left", padx="2")
+
+vdiBroker.pack(side="left", padx="5")
+vdiBroker_entry.pack(side="left", padx="5")
+vdiBroker_button.pack(side="left", padx="2")
+
+frame_bottom_1.pack(fill="x", pady="2") # add the row-cell size of 1
+frame_bottom_2.pack(fill="x", pady="2")
+frame_bottom_3.pack(fill="x", pady="2")
+
+text1.pack()# without .pack() there is no render on screen
+
+
+progressbar = Progressbar(frame_bottom_3, mode='indeterminate', length=100)
+progressbar.grid(column=1, row=0, sticky="w")
+
+tk.Button(frame_bottom_3, text="Group Unassigned", command=lambda: start_submit_thread(None)).grid(column=0, row=1, sticky="E") # this is how you call function on a separate thread
+tk.Button(frame_bottom_3, text="Group Unassigned", command=lambda: start_submit_thread(None)).grid(column=0, row=2, sticky="E") # this is how you call function on a separate thread
+
 
 window.mainloop() # End
 
--- webscrape/webscrape/SeleniumSD/automate_servicedesk.py
+++ webscrape/webscrape/SeleniumSD/automate_servicedesk.py
@@ -12,75 +12,117 @@ import pprint
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from pathlib import Path
-from time import sleep, strftime
+import traceback
+import logging
+
 
 class AutoSD:
 
     def __init__(self):
         print('initiating the bot...')
-        self.USER = 'antonoium'
-        self.PASSWORD = 'madutzu93classiC'
-        chromedriver_path = "C:\\Users\\antonoium\\Desktop\\chromedriver"
-        global driver, cookies_location
-        cookies_location = Path("C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\cookies.txt")
-        driver = webdriver.Chrome(executable_path=chromedriver_path)  # This will open the Chrome window
-        #sleep(2)
+        global USER, PASSWORD, COOKIES_PATH, COOKIES, chrome, session_ID, URL
+        USER = 'antonoium'
+        PASSWORD = 'madutzu93classiC'
+        COOKIES = "C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\webscrape\\SeleniumSD\\cookies.txt"
+        CHROMEDRIVER_PATH = "C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\webscrape\\SeleniumSD\\chromedriver.exe"
+        COOKIES_PATH = Path(COOKIES)
+        chrome = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)  # This will open the Chrome window
 
-    # This should run once per day
+        session_ID = chrome.session_id
+        URL = chrome.command_executor._url
+        #print(session_ID, URL)
 
-
-    def save_cookies(self, location):
-        self.pickle.dump(driver.get_cookies(), open(location, "wb"))
+    @staticmethod
+    def save_cookies(driver, location):
+        pickle.dump(driver.get_cookies(), open(location, "wb"))
 
     @staticmethod
     def load_cookies(driver, location, url='https://servicedesk.csiltd.co.uk/'):
-        cookies = pickle.load(open(cookies_location, "rb"))
+        cookies = pickle.load(open(location, "rb"))
         driver.delete_all_cookies()
         url = "https://google.com" if url is None else url
         driver.get(url)
         for cookie in cookies:
             driver.add_cookie(cookie)
 
-    #This is my main function to be called
     def get_sd_cookies(self):
 
-        driver.get('https://servicedesk.csiltd.co.uk/')  # Group Unassigned
+        chrome.get('https://servicedesk.csiltd.co.uk/')  # Group Unassigned
 
         user_field = "//input[@id='username']"
         password_field = "//input[@id='password']"
         login_button = "//button[@id='loginSDPage']"
 
-        user_element = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(user_field))
-        user_element.send_keys(self.USER)
+        user_element = WebDriverWait(chrome, 10).until(lambda chrome: chrome.find_element_by_xpath(user_field))
+        user_element.send_keys(USER)
 
-        password_element = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(password_field))
-        password_element.send_keys(self.PASSWORD)
+        password_element = WebDriverWait(chrome, 10).until(lambda chrome: chrome.find_element_by_xpath(password_field))
+        password_element.send_keys(PASSWORD)
 
-        login_element = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(login_button))
+        login_element = WebDriverWait(chrome, 10).until(lambda chrome: chrome.find_element_by_xpath(login_button))
         login_element.click()
 
-        pprint.pprint(driver.get_cookies())  # to visualise them in console
-        self.save_cookies(driver, "C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\cookies.txt")
-        driver.quit();
+        pprint.pprint(chrome.get_cookies())  # to visualise them in console
+        self.save_cookies(chrome, COOKIES)
+        chrome.quit()
+
+    @staticmethod
+    def inject_javascript():
+        # get script content from separate file
+        my_file = open("C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\webscrape\\SeleniumSD\\highlight_script.js", "rt")
+        contents = my_file.read()
+        my_file.close()
+        chrome.execute_script(contents) # replace with selenium doing things
+
+    @staticmethod
+    def open_assigned_to(): # This is recreation of my highlight_script.js as Selenium
+
+        assigned_to = chrome.find_element_by_xpath("//*[@id='RequestsView_TABLE']/tbody/tr[3]/td[8]")
+        currently_assigned_to = assigned_to.get_attribute("textContent").strip()
+        # print(currently_assigned_to)
+
+        if currently_assigned_to == 'Unassigned':
+            assigned_to.click()
+            return 'clicked'
+
 
 
     def main(self):
 
-        if not cookies_location.is_file():
-            self.get_sd_cookies()
+        try:
 
-        # Login to SD using cookies and getting into groups Unassigned
-        self.load_cookies(driver, cookies_location)
-        driver.get(
-            'https://servicedesk.csiltd.co.uk/WOListView.do?requestViewChanged=true&viewName=38020_MyView&globalViewName=All_Requests')
+            if not COOKIES_PATH.is_file():
+                self.get_sd_cookies() # If cookies.txt not to be found, log in and create em
 
-        # get script content from separate file
-        myfile = open("C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\webscrape\\SeleniumSD\\highlight_script.js", "rt")
-        contents = myfile.read()
-        myfile.close()
+            # Otherwise skip logging and use cookies
+            self.load_cookies(chrome, COOKIES)
+            chrome.get('https://servicedesk.csiltd.co.uk/WOListView.do?requestViewChanged=true&viewName=38020_MyView&globalViewName=All_Requests')
 
-        # Browser inject with the script defined at highlight_script.js
-        driver.execute_script(contents) # replace with selenium doing things
+            self.open_assigned_to()
+            # safe0 = self.open_assigned_to()
+            #print(safe0)
+
+            #self.inject_javascript()
+
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            # Logs the error appropriately.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         # Get Js Code from other file as string for selenium browser execute

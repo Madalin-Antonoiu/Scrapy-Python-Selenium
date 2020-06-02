a5a833c51ecaace2ccefb0e4dbb7d0f33adb6f92
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-06-02 11:10:19 +0100

    Get cookies if cookies.txt doesnt exist, else log in using cookies session:) YAY!

--- highlight_script.js
+++ highlight_script.js
@@ -1,23 +1,437 @@
-Document.prototype.queryXPath = function (path) {
+"use strict";
+//Declare functions
+function setup(){
+    Document.prototype.queryXPath = function (path) {
 
-        var list = new Array();
+    var list = new Array();
 
-        var xpath = document.evaluate(
-          path,
-          document,
-          null,
-          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-          null
-        );
+    var xpath = document.evaluate(
+        path,
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    );
 
-        for (var i=0; i<xpath.snapshotLength; i++) {
+    for (var i=0; i<xpath.snapshotLength; i++) {
 
-          list.push(xpath.snapshotItem(i));
+        list.push(xpath.snapshotItem(i));
 
-        };
+    };
 
-        return list
+    return list
+
+    }
+ 
+
+
+
+
+} setup();
+// Packages
+
+//Nice retry function
+let _track = 0;
+const delay = (ms) => new Promise(res => setTimeout(res, ms));
+async function retry(fn, n, ms) {
+    let lastError;
+
+    for (let i = 0; i < n; i++) {
+        try {
+            if(i>0){
+                console.log(`Retrying...(${i})...[every ${ms}]`)
+            }
+            await delay (ms); // Not only await the given time in main function, but also console log it
+            return await fn;
+        }
+        catch (e) {
+            lastError = e;
+
+        }
+    }
+    throw lastError;
+}
+async function simulateClick(item){
+    item.dispatchEvent(new PointerEvent('pointerdown', {bubbles: true}));
+    item.dispatchEvent(new MouseEvent('mousedown', {bubbles: true}));
+    item.dispatchEvent(new PointerEvent('pointerup', {bubbles: true}));
+    item.dispatchEvent(new MouseEvent('mouseup', {bubbles: true}));
+    item.dispatchEvent(new MouseEvent('mouseout', {bubbles: true}));
+    item.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    item.dispatchEvent(new Event('change', {bubbles: true}));
+
+    return true;
+}
+
+
+
+// Helper functions - Each function should do only one thing!
+async function inGroupUnassigned(){ //Step1
+    var my_location = document.getElementById('listview_btn');
+
+    if(my_location !== null && my_location.textContent.includes("Group = Unassigned")){
+        return {'Step 1':'Service Desk - Group Unassigned detected. Carrying on...'}
+    }
+
+    if(my_location == null){
+        throw new Error('You are not on Service Desk - Requests. Aborting.') //sau cu throw, cred ca opreste codul automat, si nu mai trebuie sa verific la urmatoarea ce vine de la prima
+    }
+
+    if(my_location !== null && !my_location.textContent.includes("Group = Unassigned")){
+        throw new Error('You are on Service Desk - Requests page, but NOT in Group Unassigned. Aborting.');
+    }
+
+    throw new Error('Exception fail - check me')
+}
+
+async function closeLeftover(){//Step2
+
+        var dialog = document.getElementById('_DIALOG_LAYER');
+
+        //Does one thing, check wether should run following or skip entirely
+
+        if (dialog == null || dialog.style.visibility == 'hidden'){ // if dialog is null or hidden, skip !
+            return {'Step2 - Skip': 'No previous popup open, skipping...'}
+        }
+        if(dialog !== null && dialog.style.visibility == 'visible'){ // close things only if dialog not null AND is visible
+            var technician = document.getElementById('s2id_selectTechnician');
+            var group = document.getElementById('s2id_assignGroup');
+
+            // Does one thing, deals with the technician/group popup
+            async function closeAssignedPopup(x){
+                var name;
+
+                if(x.id == 's2id_selectTechnician'){
+                    name = 'technician'
+                }
+                if(x.id == 's2id_assignGroup'){
+                    name = 'group'
+                }
+
+                if(x !== null ) {
+
+
+                    if(x.classList.contains('select2-dropdown-open')){
+
+                        simulateClick(x.children[0]);
+
+                        return `${name} closed`
+                    }
+
+
+                }
+
+                return `${name} not open`
+            }
+            //Does one thing, deals with open dialog
+            async function closeAssignedDialog(y){
+
+                    //if Dialog not null
+                    if (y !== null && y.style.visibility == 'visible'){
+                            //  shut it down!
+                            y.style.visibility = 'hidden';
+
+                            return 'dialog closed'
+
+                    }
+
+
+                        throw new Error('Exception, check my execution body')
+
+            }
+
+            //Calling them
+            const safe1 = await  retry (closeAssignedPopup(technician), 1, null);// call
+            const safe2 = await  retry (closeAssignedPopup(group), 1, null);// call
+            const safe3 = await  retry (closeAssignedDialog(dialog), 1, null);// call
+
+
+            return {'Step2': `Summary: ${safe1} , ${safe2} and ${safe3}`}
+        }
+
+        throw new Error('Fail  - 8dsjdks')
+
 
 }
-var tableRows = document.queryXPath("//*[@id='RequestsView_TABLE']/tbody/tr");
-console.log(tableRows);
+
+async function getElement (row_num) { //converted
+
+    const element = await document.queryXPath(`//*[@id='RequestsView_TABLE']/tbody/tr[${row_num}]`)[0];
+
+    if(element !== null){ // if element not null, return with success, else go on and return failed
+
+        return {'Step 3': 'Got row element',
+                'element': element
+                }
+    }
+
+    throw new Error('fail - 049349394')
+}
+
+// am ramas aici
+async function makeItObject (message){ //converted
+
+    var row = {};
+
+    row.element = message.element
+    row.id = message.element.cells[5].textContent.trim()
+    row.agent = message.element.cells[7].children[0]
+    row.mode = message.element.cells[17].textContent.trim()
+    row.assign = message.element.cells[7].children[0].children[0].children[0]
+
+    if(row.element !== null ){ // if element not null, return with success, else go on and return failed
+        return row
+    }
+
+    throw new Error('fail')
+
+    //}
+}
+
+async function openDialog (row) {
+
+    if (row.agent.textContent.trim().includes('Unassigned')){
+        row.agent.parentElement.style.outline = "2px solid red";
+        row.element.classList.add("rowHiliten");// highlight firstRow
+        // Highlight action and click
+
+       var openHandle = await retry ( simulateClick(row.assign) , 10, 300);// await to open the popup
+
+       row.agent.parentElement.style.outline = "2px solid gray";
+
+        return {
+                    'ticketId': row.id,
+                    'openHandle': openHandle,
+                    'message':'Clicked to open dialog.',
+                    }
+
+    }
+
+    throw new Error('Fail - AssignedTo does not contain "Unassigned" for this row.')
+
+}
+
+async function getDialogDivs (elem) {//this and getElement, might be good to combine
+    let x = {};
+
+    if (elem.message.includes('Clicked to open dialog')){
+
+        var AssignTechnicianPopup = await document.queryXPath("//div[@id='_DIALOG_LAYER']")[0];
+        var table = await document.queryXPath("//table[@id='Monthlyscan']")[0];
+        var group = await document.queryXPath("//div[@id='s2id_assignGroup']")[0]; //s2id_
+        var technician = await document.queryXPath("//div[@id='s2id_selectTechnician']")[0];
+
+        //we open the popup in the previous function
+
+        if(AssignTechnicianPopup.style.visibility === 'visible' && AssignTechnicianPopup !== null && group !== null && technician !== null){
+
+            table.style.outline='2px solid gray';
+            group.style.outline='2px solid orange';
+            technician.style.outline='2px solid violet';
+
+            let currentTechnician = technician.children[0].children[0].textContent;
+            //simulateClick(technician.children[0])
+
+            return x = {
+                         'group_div' : group,
+                         'technician_div': technician,
+                         'currentTechnician': currentTechnician,
+                         'ticketId': elem.ticketId,
+                         'AssignTechnicianPopup': AssignTechnicianPopup
+                        }
+        }
+
+        throw new Error('Fail - Could not target "Group"')
+
+    }
+
+    throw new Error('Fail - Previous did not return : Dialog is open ')
+
+};
+
+async function openGroupPopup(popup) {
+
+    if(popup.currentTechnician == 'NONE'){ // third safety measure for current ticket to be assigned to NONE, Unassigned
+
+        //if group is already open
+        if (popup.group_div.classList.contains('select2-dropdown-open')){
+            return 'Group is already open. Skipping...'
+        }
+
+         //if group is not already open
+        if(!popup.group_div.classList.contains('select2-dropdown-open')){
+            let x = {};
+                // open it
+            simulateClick(popup.group_div.children[0]); //popup.group_div.children[0] is the <a> tag to open the group popup
+            //popup.group_div.children[1] is the UL list only available if above is true, opened
+
+            return x = { 'message': 'Opened Group Popup.',
+                         'div':   popup.group_div.children[0]
+                        }
+
+        }
+
+
+
+
+    }
+
+    throw new Error('Current technician is not NONE');
+}
+
+async function selectSDGroup(){
+
+    // i could probably get rid of these 2 and target groupsList directly
+    let groupsDiv = await document.queryXPath("//div[@id='select2-drop']")[0]; //
+    let groupsUl= groupsDiv.children[1];
+    let groupsLi = groupsUl.children;
+
+
+    // Safer to do with with a for loop in case they add other groups later on
+
+    for ( i=0; i < groupsLi.length; i ++){
+
+        if(groupsLi[i].children[0].textContent == "Service Desk"){
+
+            simulateClick(groupsLi[i]);
+
+            return {'message':'Service Desk group selected',
+                    }
+
+        }
+    }
+
+    throw new Error('Failed to find SD group')
+
+}
+
+async function hiddenWoId(safe4){
+
+    let currentSelectedGroupText = document.queryXPath("//div[@id='s2id_assignGroup']//a[@class='select2-choice']")[0].children[0].textContent;
+    //if Assign technician dialog popup still open & group currently holds service desk
+
+    if(!safe4.AssignTechnicianPopup.style.visibility == "visible"){ // if not visible, abort
+        return 'ABORT. Assign Technician Popup is not open.'
+    }
+
+    if(safe4.AssignTechnicianPopup.style.visibility == "visible" && currentSelectedGroupText == 'Service Desk' ){
+        //yet another check, form submit has a hidden woID value of ticket ID- verify it against the ticketId i grabbed in previous steps
+        let formHiddenInputTicketId = document.queryXPath("//tr[@class='FormSubmitBG']")[0].children[0].children[0].value;
+
+
+
+        // if formHiddenInputTicketId = passed in ticket
+        return {'message' : 'Assign Technician is OPEN, Service Desk currently selected',
+                'formHiddenWoID': formHiddenInputTicketId,
+                'ticketId': safe4.ticketId,
+                'currentTechnician': safe4.currentTechnician,
+                'currentGroup': safe4.group_div.children[0].children[0].textContent
+                }
+    }
+
+    throw new Error('Faillll')
+
+    //if step 1 contained an agent, you need to refactor this to account for its name and its group, and only then assign. For now, just Unassigned and SD
+
+}
+
+async function assign(ticket){
+
+    let assign = await document.queryXPath("//label[@class='formsubmitbtn-label-ui1']")[0].children[0];
+    //yet another safety measure
+
+    if(ticket.ticketId == ticket.formHiddenWoID && assign !== null){
+
+
+        if(ticket.currentGroup == "Service Desk"){
+
+            //assign it
+            // update storage
+            return {'Last step':'Here i assign it',
+                    'formHiddenWoID': ticket.formHiddenWoID,
+                    'ticketId':ticket.ticketId,
+                    'currentTechnician': ticket.currentTechnician,
+                    'currentGroup': ticket.currentGroup
+                }
+        }
+
+
+
+    }
+
+    throw new Error('Failed to assign - check me.')
+}
+
+//Main function
+async function main(row_to_try) {
+    //try catch is for calls only, not for methods above
+    try{
+
+        //enforce a refresh nd reinject of all code except the call main function (from selenium)
+        const safe = await retry (inGroupUnassigned(), 10, 500); // get element will return the element from within, dont put try 0 times lol
+        console.log(await safe);
+
+        const safe0 = await retry (closeLeftover(), 2, 200); // get element will return the element from within, dont put try 0 times lol
+        console.log(await safe0);
+
+        const safe1 = await retry (getElement(row_to_try), 5, 200); // get element will return the element from within, dont put try 0 times lol
+        console.log(await safe1);
+
+        const safe2 = await retry (makeItObject(safe1), 2, 500);
+        console.log(await safe2);
+
+        const safe3 = await retry (openDialog(safe2), 2, 1000);// retry 5 times, a try each second
+        console.log(await safe3);
+
+        const safe4 = await retry (getDialogDivs(safe3), 5, 500);
+        console.log(await safe4);
+
+        const safe5 = await retry (openGroupPopup(safe4), 5, 500);
+        console.log(await safe5);
+
+        const safe6 = await retry (selectSDGroup(), 5, 500);
+        console.log(await safe6);
+
+        const safe7 = await retry (hiddenWoId(safe4), 5, 500);
+        console.log(await safe7);
+
+        const safe8 = await retry (assign(safe7), 5, 500);
+        console.log(await safe8);
+
+        storage = await safe8;
+        executed += `[${storage.ticketId} - ${storage.currentTechnician}] : Assigned to ${storage.currentGroup}` + '\n' ;
+        executed_last = `[${storage.ticketId} - ${storage.currentTechnician}] : Assigned to ${storage.currentGroup}` ;
+
+        console.log(executed)
+        console.log(` %c ${executed_last} `, 'background: #DC5B4A; color: #FFFFFF');
+
+        //console.log(` %c ${storage.ticketId} `, 'background: #222; color: #bada55' );
+        //could skip execute thing and console.log storage.ticketId with custom css
+
+    } catch(error){
+        console.log(error);
+
+        if(error == "TypeError: Cannot read property 'cells' of undefined"){
+            console.log('Go back to Group Unassigned stack!')
+        }
+        if(error == "ReferenceError: makeItObject is not defined"){
+            console.log('You are in the wrong place. You should be in Group Unassigned stack!')
+        }
+
+
+
+    }
+
+
+}
+
+var storage = {}; // this stores safe4 to be used globally
+var executed = ["Records:\n"]; // this is what holds what i executed
+var executed_last = "Last Actions Taken:\n";
+//var row_to_try = 6;
+
+main(3);
+
+
+
+// //div[@id='_DIALOG_LAYER'] display!= none
--- webscrape/webscrape/spiders/selen_test.py
+++ webscrape/webscrape/spiders/selen_test.py
@@ -1,35 +1,74 @@
+# After each one is assigned, it does a page refresh so bye bye code, it needs to be injected over and over again
+# It will throw an error if Refresh 3,5.. min is active. Needs to be on 0
+# Double verify AND log every time you do a row - check if unnassigned, when popup open, check if NONE :) This way we absolutely make sure 0% chance of error
 
+# if cookies not empty ( cookies.txt will be deleted automatically from drive every 12 hours)
+# run the short, load way
+
+# Delete cookies.txt for fresh cookies if it fails to work
+
+import pickle
+import pprint
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from pathlib import Path
 
-PATH = 'C:\\Users\\antonoium\\Desktop\\chromedriver'
-USER = 'antonoium'
-PASSWORD = 'madutzu93classiC'
+# This should run once per day
+cookies_location = Path("C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\cookies.txt")
+
+def save_cookies(driver, location):
+    pickle.dump(driver.get_cookies(), open(location, "wb"))
+
+
+def load_cookies(driver, location, url='https://servicedesk.csiltd.co.uk/'):
+    cookies = pickle.load(open(cookies_location, "rb"))
+    driver.delete_all_cookies()
+    url = "https://google.com" if url is None else url
+    driver.get(url)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+
+def get_sd_cookies():
+    PATH = 'C:\\Users\\antonoium\\Desktop\\chromedriver'
+    USER = 'antonoium'
+    PASSWORD = 'madutzu93classiC'
+
+    driver = webdriver.Chrome('C:\\Users\\antonoium\\Desktop\\chromedriver.exe')
+    driver.get('https://servicedesk.csiltd.co.uk/')  # Group Unassigned
+
+    user_field = "//input[@id='username']"
+    password_field = "//input[@id='password']"
+    login_button = "//button[@id='loginSDPage']"
+
+    UserElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(user_field))
+    UserElement.send_keys(USER)
+
+    PasswordElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(password_field))
+    PasswordElement.send_keys(PASSWORD)
+
+    LoginElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(login_button))
+    LoginElement.click()
+
+    pprint.pprint(driver.get_cookies())  # to visualise them in console
+    save_cookies(driver, "C:\\Users\\antonoium\\Desktop\\venv\\webscrape\\cookies.txt")
+    driver.quit();
+
+
+if not cookies_location.is_file():
+    get_sd_cookies()
 
 driver = webdriver.Chrome('C:\\Users\\antonoium\\Desktop\\chromedriver.exe')
-driver.get('https://servicedesk.csiltd.co.uk/WOListView.do?requestViewChanged=true&viewName=38020_MyView&globalViewName=All_Requests') # Group Unassigned
+load_cookies(driver, cookies_location)
+driver.get(
+    'https://servicedesk.csiltd.co.uk/WOListView.do?requestViewChanged=true&viewName=38020_MyView&globalViewName=All_Requests')
 
-user_field = "//input[@id='username']"
-password_field = "//input[@id='password']"
-login_button = "//button[@id='loginSDPage']"
-
-UserElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(user_field))
-UserElement.send_keys(USER)
-
-PasswordElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(password_field))
-PasswordElement.send_keys(PASSWORD)
-
-LoginElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(login_button))
-LoginElement.click()
-
-driver.get('https://servicedesk.csiltd.co.uk/WOListView.do?requestViewChanged=true&viewName=38020_MyView&globalViewName=All_Requests')
-# driver.execute_script("Document.prototype.queryXPath = function (path) { var list = new Array(); var xpath = document.evaluate( path, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null ); for (var i=0; i<xpath.snapshotLength; i++) { list.push(xpath.snapshotItem(i)); }; return list }; var tableRows = document.queryXPath(`//*[@id='RequestsView_TABLE']/tbody/tr`); console.log(tableRows);")
-
+# get script content from separate file
 myfile = open("C:\\Users\\antonoium\\Desktop\\venv\\highlight_script.js", "rt")
 contents = myfile.read()
 myfile.close()
-print(contents)
+
+# Browser inject with the script defined at highlight_script.js
 driver.execute_script(contents)
 
 # Get Js Code from other file as string for selenium browser execute
